1c2e6f52a62cb080a4b52c52d2edd92b0294699c
TTTTMMMM <junque135@gmail.com>
2020-07-26 22:57:50 -0400

    animation a w-i-p

--- src/assets/js/components/MainComponents/CanvasAnimationComponent.tsx
+++ src/assets/js/components/MainComponents/CanvasAnimationComponent.tsx
@@ -27,9 +27,6 @@ class CanvasAnimationComponent extends Component<{}, {}> {
    animate() {
       requestAnimationFrame(this.animate);
       this.c!.clearRect(0, 0, this.cW, this.cH);
-      // this.x = Math.random() * this.cW;
-      // this.y = Math.random() * this.cH;
-      // this.radius = Math.random() * 25;
       this.c!.beginPath();
       this.c!.strokeStyle = "rgba(255, 0, 0, .78)";
       this.c!.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
@@ -47,22 +44,22 @@ class CanvasAnimationComponent extends Component<{}, {}> {
    getCanvasContent() {
       let c = this.c;
       this.canvasElement = this.canvasRef.current;
-      this.cW = window.innerWidth - 25;
-      this.cH = window.innerHeight - 75;
-      var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
-      this.deltaX = Math.round(plusOrMinus * this.deltaX);
-      var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
-      this.deltaY = Math.round(plusOrMinus * this.deltaY);
-      this.x = this.x > this.cW ? Math.round(this.cW / 1.5) : this.x;
-      this.y = this.y > this.cH ? Math.round(this.cH / 2) : this.y;
-
-      console.log(`r: ${this.radius}`);
-      console.log(`dX: ${this.deltaX}`);
-      console.log(`dY: ${this.deltaY}`);
-      console.log(`x: ${this.x}; this.cW: ${this.cW}`);
-      console.log(`y: ${this.y}; this.cH: ${this.cH}`);
       if (this.canvasElement && this.haventCreatedCanvasYet) {
          this.haventCreatedCanvasYet = false;
+         this.cW = window.innerWidth - 25;
+         this.cH = window.innerHeight - 75;
+         var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+         this.deltaX = Math.round(plusOrMinus * this.deltaX);
+         var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+         this.deltaY = Math.round(plusOrMinus * this.deltaY);
+         this.x = this.x > this.cW ? Math.round(this.cW / 1.5) : this.x;
+         this.y = this.y > this.cH ? Math.round(this.cH / 2) : this.y;
+
+         console.log(
+            `%cr:${this.radius}  dX:${this.deltaX}  dY:${this.deltaY}`,
+            "background:white; border: 3px solid green; margin: 2px; padding: 3px; color:blue;"
+         );
+
          this.canvasElement.setAttribute("height", this.cH.toString());
          this.canvasElement.setAttribute("width", this.cW.toString());
          c = this.canvasElement!.getContext("2d")!;
