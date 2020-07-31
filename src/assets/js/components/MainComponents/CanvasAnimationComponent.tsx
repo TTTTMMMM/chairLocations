bac24070bca6e676b2832a3ddfc827927cc9a10c
TTTTMMMM <junque135@gmail.com>
2020-07-31 10:48:44 -0400

    animation: done

--- src/assets/js/components/MainComponents/CanvasAnimationComponent.tsx
+++ src/assets/js/components/MainComponents/CanvasAnimationComponent.tsx
@@ -132,7 +132,6 @@ class CanvasAnimationComponent extends Component<{}, {}> {
    c: CanvasRenderingContext2D | undefined;
    cW: number = 0; // canvasWidth
    cH: number = 0; // canvasHeight
-   haventCreatedCanvasYet: boolean = true;
 
    animate() {
       requestAnimationFrame(this.animate);
@@ -140,10 +139,18 @@ class CanvasAnimationComponent extends Component<{}, {}> {
       this.circleArray.forEach((circle) => {
          circle.update(this.cW, this.cH, this.c, this.colorArray);
       });
-      this.c!.beginPath();
       this.c!.lineWidth = 2;
+      this.c!.beginPath();
       this.c!.moveTo(this.circleArray[0].x, this.circleArray[0].y);
       this.c!.lineTo(this.circleArray[1].x, this.circleArray[1].y);
+      this.c!.stroke();
+      this.c!.beginPath();
+      this.c!.moveTo(this.circleArray[1].x, this.circleArray[1].y);
+      this.c!.lineTo(this.circleArray[2].x, this.circleArray[2].y);
+      this.c!.stroke();
+      this.c!.beginPath();
+      this.c!.moveTo(this.circleArray[2].x, this.circleArray[2].y);
+      this.c!.lineTo(this.circleArray[0].x, this.circleArray[0].y);
       this.c!.stroke();
       this.c!.font = "15px Josefin Sans";
       this.c!.fillStyle = "#000000";
@@ -165,53 +172,50 @@ class CanvasAnimationComponent extends Component<{}, {}> {
       this.c!.drawImage(this.shImage, 8, 40, 50, 59);
    }
 
-   getCanvasContent() {
+   componentDidMount() {
       this.canvasElement = this.canvasRef.current;
       this.shImage = new Image();
       this.shImage.src = "/images/sunstar.png";
-      if (this.canvasElement && this.haventCreatedCanvasYet) {
-         this.haventCreatedCanvasYet = false;
-         this.cW = window.innerWidth - 25;
-         this.cH = window.innerHeight - 75;
-         this.canvasElement.setAttribute("height", this.cH.toString());
-         this.canvasElement.setAttribute("width", this.cW.toString());
-         this.c = this.canvasElement!.getContext("2d")!;
-         let x: number = 0;
-         let y: number = 0;
-         let dX: number = 0;
-         let dY: number = 0;
-         let radius: number = 0;
-         let colorIndex: number = 0;
-         this.colorBank = Math.floor(Math.random() * this.numColorBanks);
-         for (let i = 0; i < 259; i++) {
-            radius = Math.ceil(Math.random() * (8 - 1) + 1); // random # between 2 and 9
-            x = Math.ceil(
-               Math.random() * (window.innerWidth - radius * 3) + radius
-            );
-            y = Math.ceil(
-               Math.random() * (window.innerHeight - radius * 3) + radius
-            );
-            x = x >= this.cW - radius ? Math.round(this.cW / 1.5) : x;
-            y = y >= this.cH - radius ? Math.round(this.cH / 2) : y;
-            dX = Math.round(Math.random() * (3 - 1) + 1);
-            dY = Math.round(Math.random() * (3 - 1) + 1);
-            var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
-            dX = Math.round(plusOrMinus * dX);
-            var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
-            dY = Math.round(plusOrMinus * dY);
-            colorIndex = 5 * this.colorBank + Math.floor(Math.random() * 5);
-            this.circleArray.push(
-               new Circle(x, y, radius, dX, dY, this.colorBank, colorIndex)
-            );
-         }
-         this.animate();
+      console.log(`in componentDidMount(), this.canvasElement-> `);
+      console.dir(this.canvasElement);
+      this.cW = window.innerWidth - 25;
+      this.cH = window.innerHeight - 75;
+      this.canvasElement!.setAttribute("height", this.cH.toString());
+      this.canvasElement!.setAttribute("width", this.cW.toString());
+      this.c = this.canvasElement!.getContext("2d")!;
+      let x: number = 0;
+      let y: number = 0;
+      let dX: number = 0;
+      let dY: number = 0;
+      let radius: number = 0;
+      let colorIndex: number = 0;
+      this.colorBank = Math.floor(Math.random() * this.numColorBanks);
+      for (let i = 0; i < 259; i++) {
+         radius = Math.ceil(Math.random() * (8 - 1) + 1); // random # between 2 and 9
+         x = Math.ceil(
+            Math.random() * (window.innerWidth - radius * 3) + radius
+         );
+         y = Math.ceil(
+            Math.random() * (window.innerHeight - radius * 3) + radius
+         );
+         x = x >= this.cW - radius ? Math.round(this.cW / 1.5) : x;
+         y = y >= this.cH - radius ? Math.round(this.cH / 2) : y;
+         dX = Math.round(Math.random() * (3 - 1) + 1);
+         dY = Math.round(Math.random() * (3 - 1) + 1);
+         var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+         dX = Math.round(plusOrMinus * dX);
+         var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+         dY = Math.round(plusOrMinus * dY);
+         colorIndex = 5 * this.colorBank + Math.floor(Math.random() * 5);
+         this.circleArray.push(
+            new Circle(x, y, radius, dX, dY, this.colorBank, colorIndex)
+         );
       }
-
-      return <canvas style={canvasStyling} ref={this.canvasRef} />;
+      this.animate();
    }
 
    render() {
-      return <>{this.getCanvasContent()}</>;
+      return <canvas style={canvasStyling} ref={this.canvasRef} />;
    }
 }
 
